e1832e5eb4f33c27c00ea2ae2d4f458716000d2c
Tilen Kelc <tilen.kelc@gmail.com>
2020-01-08 10:00:29 +0100

    Adding Google Login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -10,9 +10,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
         redirect_to new_user_registration_url
       end
     end
-    
+
     def google_oauth2
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-
-  devise :omniauthable, omniauth_providers: [:google_oauth2]
-  devise :omniauthable, :omniauth_providers => [:facebook]
+         
+         
+  devise :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
 
   has_many :feeds, dependent: :destroy
   acts_as_commontator
@@ -28,23 +27,16 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.username = auth.info.name   # assuming the user model has a name
-      user.avatar = auth.info.image_url # assuming the user model has an image
+      user.provider = "facebook"
     end
   end
 
   def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
-
-    # Uncomment the section below if you want users to be created if they don't exist
-    unless user
-      user = User.create(username: data['name'],
-      email: data['email'],
-      password: Devise.friendly_token[0,20],
-      avatar: data['image'],
-      provider: 'facebook'
-    )
+    where(provider: access_token.provider, uid: access_token.uid).first_or_create do |user|
+      user.email = access_token.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.username = access_token.info.name   # assuming the user model has a name
+      user.provider = "google"
     end
-    user
   end
 end
