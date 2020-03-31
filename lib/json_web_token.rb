a833677d2a3c98fea23dc644bfae9e972e472059
Tilen <tilen.kelc@github.com>
2020-03-31 15:38:14 +0200

    Fix

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,7 +2,7 @@ class JsonWebToken
   class << self
     def encode(payload, expiry = 24.hours.from_now)
       payload[:expiry] = expiry.to_i
-      secret_key_base = Rails.application.secrets.secret_key_base
+      secret_key_base = ENV["JWT_SECRET"]
       JWT.encode(payload, secret_key_base)
     end
 
