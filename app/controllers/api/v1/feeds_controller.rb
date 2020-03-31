87673401a8890650605215dec6ca384aedfdd557
Tilen <tilen.kelc@github.com>
2020-03-31 15:35:55 +0200

    Fix

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::FeedsController < ActionController::API
   before_action :set_feed, only: [:index, :edit, :update, :destroy]
+  before_action :authenticate_request, only: [:index]
 
   def index
     feeds = Feed.all
     feeds = feeds.map do |feed|
-      { id: feed.id, text: feed.text, picture: feed.picture, time: feed.time, reported: feed.reported, user_id: feed.user.id }
+      { id: feed.id, text: feed.text, picture: feed.picture, time: feed.time, reported: feed.reported, user_id: feed.user_id }
     end
 
     render json: { results: feeds }.to_json, status: :ok
@@ -12,8 +13,7 @@ class Api::V1::FeedsController < ActionController::API
 
   def create
     @feed = Feed.new(feed_params)
-    @feed.user_id = current_user.id
-    image = params[:feed][:image] if params[:feed][:image].present?
+    @feed.user_id = @current_api_user
     if @feed.save
       render json: { result: @feed }.to_json, status: :ok
     else
@@ -33,9 +33,9 @@ class Api::V1::FeedsController < ActionController::API
   def update
     if @feed
       @feed.update(feed_params)
-      render json: { result: @feed }.to_json, status: :ok
+      render json: { result: @feeds }.to_json, status: :ok
     else
-      render json: { result: @feed.errors }.to_json, status: :unprocessable_entity
+      render json: { result: @feeds.errors }.to_json, status: :unprocessable_entity
     end
   end
 
@@ -47,6 +47,6 @@ class Api::V1::FeedsController < ActionController::API
   end
 
   def feed_params
-    params.require(:feed).permit(:text, :time, :reported, :image)
+    params.permit(:text, :time, :reported, :image)
   end
 end
