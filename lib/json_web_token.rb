5fdb6e8f46557704c5d6731afcd93baba719339e
Tilen <tilen.kelc@github.com>
2020-03-31 15:56:01 +0200

    Update json_web_token.rb

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -3,18 +3,18 @@ class JsonWebToken
     def encode(payload, expiry = 24.hours.from_now)
       payload[:expiry] = expiry.to_i
       if Rails.env.production?
-        secret_key_base = Rails.application.secrets.secret_key_base
-      else
         secret_key_base = ENV["SECRET_KEY_BASE"]
+      else
+        secret_key_base = Rails.application.secrets.secret_key_base
       end
       JWT.encode(payload, secret_key_base)
     end
 
     def decode(token)
       if Rails.env.production?
-        secret_key_base = Rails.application.secrets.secret_key_base
-      else
         secret_key_base = ENV["SECRET_KEY_BASE"]
+      else
+        secret_key_base = Rails.application.secrets.secret_key_base
       end
       body = JWT.decode(token, secret_key_base)[0]
       HashWithIndifferentAccess.new body
